12b0a730b82d54ec2e3288d20cf91ee5899fab53
Robert Andersson <rob@ernell.se>
2016-01-10 04:22:01 +0100

    Create ControlCharacters.java

---
+++ ControlCharacters.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2015 Robert Andersson <http://www.ernell.se>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.ernell.utils;
+/**
+* <h1>Control characters</h1>
+* Control characters by name.
+*
+* @author  rob@ernell.se
+* @version 1.0
+* @since   2015-12-01
+*/
+public class ControlCharacters {
+
+	public final static char NUL = 0x000; // NULL
+	public final static char SOH = 0x001; // Start Of Heading
+	public final static char STX = 0x002; // Start of Text
+	public final static char ETX = 0x003; // End of Text
+	public final static char EOT = 0x004; // End Of Transmission
+	public final static char ENQ = 0x005; // Enquiry
+	public final static char ACK = 0x006; // Acknowledge
+	public final static char BEL = 0x007; // Bell
+	public final static char BS  = 0x008; // Backspace
+	public final static char TAB = 0x009; // Horizontal Tab
+	public final static char LF  = 0x00a; // Line Feed
+	public final static char VT  = 0x00b; // Vertical Tab
+	public final static char FF  = 0x00c; // Form Feed
+	public final static char CR  = 0x00d; // Carriage Return
+	public final static char SO  = 0x00e; // Shift Out
+	public final static char SI  = 0x00f; // Shift In
+	public final static char DLE = 0x010; // Data Link Escape
+	public final static char DC1 = 0x011; // Device Control 1
+	public final static char DC2 = 0x012; // Device Control 2
+	public final static char DC3 = 0x013; // Device Control 3
+	public final static char DC4 = 0x014; // Device Control 4
+	public final static char NAK = 0x015; // Negative Acknowledge
+	public final static char SYN = 0x016; // Synchronous Idle
+	public final static char ETB = 0x017; // End of Transmission Block
+	public final static char CAN = 0x018; // Cancel
+	public final static char EM  = 0x019; // End of Medium
+	public final static char SUB = 0x01a; // Substitute
+	public final static char ESC = 0x01b; // Escape
+	public final static char FS  = 0x01c; // File Separator
+	public final static char GS  = 0x01d; // Group Separator
+	public final static char RS  = 0x01e; // Record Separator
+	public final static char US  = 0x01f; // Unit Separator
+
+}
